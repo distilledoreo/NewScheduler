28a2d2f142aa607509b9a27a8e52bfa144350d75
distilledoreo <distilledoreo@outlook.com>
2025-08-14 00:10:46 -0400

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ["@fluentui/react-components", "@fluentui/react-icons"],
+    exclude: ["sql.js"], // sql.js ships ESM/wasm; avoid prebundling issues
+  },
+  assetsInclude: ["**/*.wasm"],
+  build: {
+    sourcemap: false,
+    target: "es2020",
+    outDir: "dist",
+  },
+});
