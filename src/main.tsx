a96fb566391172866d860aa6b828156c15da11d1
distilledoreo <distilledoreo@outlook.com>
2025-08-16 00:34:52 -0400

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { FluentProvider, teamsDarkTheme, teamsLightTheme } from "@fluentui/react-components";
+import { FluentProvider, webDarkTheme, webLightTheme } from "@fluentui/react-components";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -9,7 +9,7 @@ const root = createRoot(container);
 const Root = () => {
   const [mode, setMode] = React.useState<"light" | "dark">("light");
   const toggleTheme = () => setMode(m => (m === "light" ? "dark" : "light"));
-  const theme = mode === "light" ? teamsLightTheme : teamsDarkTheme;
+  const theme = mode === "light" ? webLightTheme : webDarkTheme;
   return (
     <FluentProvider theme={theme}>
       <App theme={mode} toggleTheme={toggleTheme} />
